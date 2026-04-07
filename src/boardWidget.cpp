0f022913e43d6662f5d78010135aef962795c491
mar <mosermauer@gmail.com>
2026-04-07 13:43:32 +0200

    proper Reset fix

--- src/boardWidget.cpp
+++ src/boardWidget.cpp
@@ -27,6 +27,10 @@ BoardWidget::BoardWidget(QWidget* parent)
 
 void BoardWidget::initializeBoardAndPieces()
 {
+    this->currentPlayer = &whitePlayer;
+    pieceSelected = false;
+    selectedSquare = {-1,-1};
+
     for (int i = 0; i < 8; ++i)
     {
         for (int j = 0; j < 8; ++j)
